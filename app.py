91ee5f01823725c5af37d6669b596854275acc6a
Emory Walsh <ewalsh00@stuy.edu>
2019-10-22 08:10:46 -0400

    Added file for app

---
+++ app.py
@@ -0,0 +1,12 @@
+#Team Noobpedia
+#Softev1 pd1
+#P00
+#2019-10-28
+
+from flask import Flask, render_template, request, redirect, url_for, session
+app = Flask(__name__)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
