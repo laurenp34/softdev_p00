e3e6ae329c39f3aca09129e3b748c6c322e96668
pollsmor <kevinli4321@gmail.com>
2019-10-22 19:54:02 -0400

    Got the secret key thing working

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
-#Team Noobpedia
-#Softev1 pd1
+#Team Noobpedia: Raymond Lee, Kevin Li, Emory Walsh
+#SoftDev1 pd1
 #P00 -- Da Art of Storytellin'
 #2019-10-28
 
 from flask import Flask, render_template, request, redirect, url_for, session
+import os #for generating a secret key
+
 app = Flask(__name__)
 
+#Secret key handling
+secret_key_file = 'secret_key.txt'
+if (os.path.exists(secret_key_file)): #check if secret key file already exists
+    file = open(secret_key_file, 'r')
+    app.secret_key = file.read()
+else: #not adding the secret key file, so generate one on the spot for ppl without it
+    file = open(secret_key_file, 'w+') #w+ creates the file if it doesn't exist
+    file.write(str(os.urandom(32)))
+    app.secret_key = file.read()
+
+file.close()
 
 if __name__ == "__main__":
     app.debug = True
